9c6df37180822065f8a51d3424ebe45c49c1745d
staranenko <sergey.taranenko@gmail.com>
2024-12-04 15:21:50 +0300

    Add get, post and delete hotels

--- main.py
+++ main.py
@@ -3,6 +3,46 @@ import uvicorn
 
 app = FastAPI()
 
+hotels = [
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Dubai", "name": "dubai"},
+]
+
+
+@app.get("/hotels")
+def get_hotels(
+    id: int | None = Query(None),
+    title: str | None = Query(None, description="The tit le of the hotel"),
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+
+@app.post("/hotels")
+def create_hotel(title: str = Body(embed=True)):
+    global hotels
+    hotels.append(
+        {
+            "id": hotels[-1]["id"] + 1,
+            "title": title,
+        }
+    )
+    return {"success": "OK"}
+
+
+@app.delete("/hotels/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    print(hotels)
+    return {"status": "OK"}
+
 
 @app.get("/")
 def func():
