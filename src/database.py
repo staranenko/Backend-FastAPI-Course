b7f3eaafeb6d90c2370bd071ef2de6aa0a8fdb07
staranenko <sergey.taranenko@gmail.com>
2024-12-06 23:12:03 +0300

    Создать менеджер сессий для работы с базой данных

--- src/database.py
+++ src/database.py
@@ -1,9 +1,8 @@
-import asyncio
-
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy import text
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from src.config import settings
 
 
 engine = create_async_engine(settings.DB_URL)
+
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
