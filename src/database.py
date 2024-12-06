74931343721d6930a842f29c894cdfe0fdd31720
staranenko <sergey.taranenko@gmail.com>
2024-12-06 22:47:35 +0300

    Добавить подключение к базе данных и проверить что оно работает вызвав версию движка базы данных

---
+++ src/database.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import text
+
+from src.config import settings
+
+
+engine = create_async_engine(settings.DB_URL)
+
+
+async def func():
+    async with engine.begin() as conn:
+        res = await conn.execute(text("SELECT version()"))
+        print(res.fetchone())
+
+asyncio.run(func())
--- src/main.py
+++ src/main.py
@@ -11,10 +11,8 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
-from src.config import settings
 
-
-print(f"{settings.DB_NAME=}")
+from src.database import *
 
 app = FastAPI(docs_url=None)
 
