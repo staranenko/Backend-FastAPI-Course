09a2971918b03e46da501a7e2f50b40298b56847
staranenko <sergey.taranenko@gmail.com>
2024-12-06 21:52:07 +0300

    Дополнить пути для запуска проекта из терминала

--- src/main.py
+++ src/main.py
@@ -5,7 +5,12 @@ from fastapi.openapi.docs import (
 )
 import uvicorn
 
-from src.hotels import router as router_hotels
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from src.api.hotels import router as router_hotels
 
 app = FastAPI(docs_url=None)
 
