3386644bf2eba034094a84db986e0aad1df0e63d
staranenko <sergey.taranenko@gmail.com>
2024-12-09 18:31:52 +0300

    Форматировать модуль с black

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings ):
+class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
@@ -12,7 +12,7 @@ class Settings(BaseSettings ):
     def DB_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file='.env')
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
