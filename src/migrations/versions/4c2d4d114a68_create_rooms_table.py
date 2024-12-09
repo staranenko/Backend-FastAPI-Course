f45d65373965728acd8d5697357c5bc7f026c7e9
staranenko <sergey.taranenko@gmail.com>
2024-12-09 17:25:45 +0300

    Сделать красивый вид в файле миграции для таблицы номеров

--- src/migrations/versions/4c2d4d114a68_create_rooms_table.py
+++ src/migrations/versions/4c2d4d114a68_create_rooms_table.py
@@ -20,14 +20,14 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     op.create_table('rooms',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('hotel_id', sa.Integer(), nullable=False),
-    sa.Column('title', sa.String(), nullable=False),
-    sa.Column('description', sa.String(), nullable=True),
-    sa.Column('price', sa.Integer(), nullable=False),
-    sa.Column('quantity', sa.Integer(), nullable=False),
-    sa.ForeignKeyConstraint(['hotel_id'], ['hotels.id'], ),
-    sa.PrimaryKeyConstraint('id')
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('hotel_id', sa.Integer(), nullable=False),
+        sa.Column('title', sa.String(), nullable=False),
+        sa.Column('description', sa.String(), nullable=True),
+        sa.Column('price', sa.Integer(), nullable=False),
+        sa.Column('quantity', sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(['hotel_id'], ['hotels.id'], ),
+        sa.PrimaryKeyConstraint('id')
     )
 
 
