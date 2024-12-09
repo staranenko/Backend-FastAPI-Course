337e96f690ad44782311611f801d2e509b8b655d
staranenko <sergey.taranenko@gmail.com>
2024-12-09 17:17:32 +0300

    Создать и применить миграцию для таблицы номеров

---
+++ src/migrations/versions/4c2d4d114a68_create_rooms_table.py
@@ -0,0 +1,35 @@
+"""Create rooms table
+
+Revision ID: 4c2d4d114a68
+Revises: e6b1ef12bbc2
+Create Date: 2024-12-09 17:12:38.836912
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '4c2d4d114a68'
+down_revision: Union[str, None] = 'e6b1ef12bbc2'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table('rooms',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('hotel_id', sa.Integer(), nullable=False),
+    sa.Column('title', sa.String(), nullable=False),
+    sa.Column('description', sa.String(), nullable=True),
+    sa.Column('price', sa.Integer(), nullable=False),
+    sa.Column('quantity', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['hotel_id'], ['hotels.id'], ),
+    sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('rooms')
