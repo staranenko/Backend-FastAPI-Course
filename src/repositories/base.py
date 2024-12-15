90d2a0ca08d232dcd933db113b165bbe5dc12590
staranenko <sergey.taranenko@gmail.com>
2024-12-15 19:44:42 +0300

    Изменить имя выражения при обновлении отеля

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -27,8 +27,8 @@ class BaseRepository:
         return result.scalars().one()
 
     async def edit(self, data: BaseModel, **filter_by) -> None:
-        edit_stmt = update(self.model).filter_by(**filter_by).values(**data.model_dump())
-        await self.session.execute(edit_stmt)
+        update_stmt = update(self.model).filter_by(**filter_by).values(**data.model_dump())
+        await self.session.execute(update_stmt)
 
     async def delete(self, **filter_by) -> None:
         delete_stmt = delete(self.model).filter_by(**filter_by)
