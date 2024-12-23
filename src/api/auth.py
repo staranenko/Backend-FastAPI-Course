867fec54e51dc7b2008815fffd090c233fb3f6ef
staranenko <sergey.taranenko@gmail.com>
2024-12-23 21:01:27 +0300

    Создать ручку для получения данные текущего пользователя по его токену доступа

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Response, Request
 
+from src.api.dependecies import UserIdDep
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UsersAdd
@@ -38,13 +39,10 @@ async def login_user(
         return {"access_token": access_token}
 
 
-@router.get("/only_auth", description="Показать токен доступа")
-async def only_auth(
-        request: Request,
+@router.get("/me", description="Показать данные пользователя")
+async def get_me(
+        user_id: UserIdDep
 ):
-    access_token = request.cookies.get("access_token", None)
-    data = AuthService().decode_token(access_token)
-    user_id = data["user_id"]
     async with async_session_maker() as session:
         user = await UsersRepository(session).get_one_or_none(id=user_id)
         return user
--- src/services/auth.py
+++ src/services/auth.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone, timedelta
+from fastapi import HTTPException
 
 from passlib.context import CryptContext
 import jwt
@@ -23,4 +24,7 @@ class AuthService:
         return self.pwd_context.verify(plain_password, hashed_password)
 
     def decode_token(self, token: str) -> dict:
-        return jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
+        try:
+            return jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
+        except jwt.exceptions.DecodeError:
+            raise HTTPException(status_code=401, detail="Неверный токен доступа")
